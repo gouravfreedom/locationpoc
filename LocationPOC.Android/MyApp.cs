97b1affb22779f53de92f9397434bb4525d5725e
gouravfreedom <gouravfreedom@gmail.com>
2020-06-13 14:53:20 +0530

    Integrated android

---
+++ LocationPOC.Android/MyApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace LocationPOC.Droid
+{
+    [Application]
+    [MetaData("com.google.android.maps.v2.API_KEY",
+              Value = "AIzaSyCOzvH-CHkEFa4Od7msB5xFaG4vK-UVuzo")]
+    public class MyApp : Application
+    {
+        public MyApp(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+    }
+}
+
