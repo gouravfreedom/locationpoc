d04b668f02ce7ee5188020b108838a11e84b97c7
gouravfreedom <gouravfreedom@gmail.com>
2020-06-13 11:14:08 +0530

    minor android changes

--- LocationPOC.Android/MainActivity.cs
+++ LocationPOC.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.GoogleMaps.Android;
 
 namespace LocationPOC.Droid
 {
@@ -21,6 +22,13 @@ namespace LocationPOC.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            var platformConfig = new PlatformConfig
+            {
+                BitmapDescriptorFactory = new CachingNativeBitmapDescriptorFactory()
+            };
+
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState, platformConfig);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
