a63ad8acedfcb8972f0a49dafefe6777e3caad4b
gp-memosoft <88316985+gp-memosoft@users.noreply.github.com>
2022-05-10 08:20:05 +0530

    Current location working

--- LocationPOC.Android/MyApp.cs
+++ LocationPOC.Android/MyApp.cs
@@ -6,7 +6,7 @@ namespace LocationPOC.Droid
 {
     [Application]
     [MetaData("com.google.android.maps.v2.API_KEY",
-              Value = "AIzaSyCOzvH-CHkEFa4Od7msB5xFaG4vK-UVuzo")]
+              Value = "AIzaSyCYvJLIo-GG4xkud--HWgVb83xsVZYmwQ8")]
     public class MyApp : Application
     {
         public MyApp(IntPtr javaReference, JniHandleOwnership transfer)
